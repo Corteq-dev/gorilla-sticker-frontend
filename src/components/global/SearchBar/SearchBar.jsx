4fc06faf2c448e615594d848bc0fda8887b1ac74
Vlady <vlady@onlinedata.dk>
2023-10-06 18:11:22 +0200

    Minor fix in searchbar

--- src/components/global/SearchBar/SearchBar.jsx
+++ src/components/global/SearchBar/SearchBar.jsx
@@ -1,19 +1,14 @@
 import { Container } from "react-bootstrap";
 import styles from "./SearchBart.module.scss";
-import InputGroupText from "react-bootstrap/InputGroupText";
 
 export default function SearchBar() {
   return (
     <Container className={styles.wrapper}>
-      <Container className={styles.container}>
-        <Container className={styles.search_image}></Container>
-        <input
-          className={styles.searchbar}
-          type="text"
-          placeholder="Search for stickers"
-        />
-        <Container className={styles.info_box}></Container>
-      </Container>
+      <div className={styles.container}>
+        <div className={styles.searchImage}></div>
+        <input className={styles.searchBar} type="text" placeholder="Search for stickers" />
+        <div className={styles.infoBox}></div>
+      </div>
     </Container>
   );
 }
