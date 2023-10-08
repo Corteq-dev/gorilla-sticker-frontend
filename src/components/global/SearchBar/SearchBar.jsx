cb51cdd721bb7c8ee3580c1b783955f1b4bfa247
Denis <prokhorend@gmail.com>
2023-10-08 18:30:33 +0300

    added menu options

--- src/components/global/SearchBar/SearchBar.jsx
+++ src/components/global/SearchBar/SearchBar.jsx
@@ -1,18 +1,29 @@
-import { Container } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import styles from "./SearchBart.module.scss";
 
 export default function SearchBar() {
-  return (
+    return (
+
     <Container className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.searchImage}></div>
-        <input
-          className={styles.searchBar}
-          type="text"
-          placeholder="Search for stickers"
-        />
-        <div className={styles.infoBox}></div>
-      </div>
+        <div className={styles.searchBar}>
+            <div className={styles.searchImage}></div>
+            <input
+                className={styles.searchBox}
+                type="text"
+                placeholder="Search for stickers"
+            />
+            <div className={styles.infoBox}></div>
+        </div>
+        <div className={styles.menuBox}>
+            <div className={styles.menuItemBox}>
+                Новинки
+            </div>
+            <div className={styles.menuItemBox}>
+                Популярные
+            </div>
+        </div>
     </Container>
-  );
+
+
+);
 }
