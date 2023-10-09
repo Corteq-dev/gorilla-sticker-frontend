b7513e0de7d444031e9875b38ffd501a07ba74a5
Denis <prokhorend@gmail.com>
2023-10-09 23:36:17 +0300

    tried to add navbar animation.

--- src/contexts/HideShowMenu.jsx
+++ src/contexts/HideShowMenu.jsx
@@ -1,18 +1,44 @@
-// import { useEffect, useRef } from "react";
-//
-// export const useObserver = (ref, canLoad, isLoading, callback) => {
-//   const observer = useRef();
-//
-//   useEffect(() => {
-//     if (isLoading) return;
-//     if (observer.current) observer.current.disconnect();
-//
-//     var cb = function (entries, observer) {
-//       if (entries[0].isIntersecting && canLoad) {
-//         callback();
-//       }
-//     };
-//     observer.current = new IntersectionObserver(cb);
-//     observer.current.observe(ref.current);
-//   }, [isLoading]);
-// };
+import React, { useState, useEffect } from "react";
+import styles from "../components/global/SearchBar/SearchBart.module.scss";
+import { Container } from "react-bootstrap";
+const Navbar = () => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      setShow(false);
+    } else {
+      setShow(true);
+
+      setLastScrollY(window.scrollY);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+  console.log(show);
+  return (
+    <Container className={styles.wrapper}>
+      <div className={styles.searchBar}>
+        <div className={styles.searchImage}></div>
+        <input
+          className={styles.searchBox}
+          type="text"
+          placeholder="Search for stickers"
+        />
+        <div className={styles.infoBox}></div>
+      </div>
+      <div className={`styles.menuBox ${show && "styles.menuBoxHiden"}`}>
+        <div className={styles.menuItemBox}>Новинки</div>
+        <div className={styles.menuItemBox}>Популярные</div>
+      </div>
+    </Container>
+  );
+};
+
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import IndexPage from "../components/page/Index/IndexPage";
 import SearchBar from "../components/global/SearchBar/SearchBar";
 import { StickerProvider } from "../contexts/StickerContext";
-import { ScrollTracker } from "../contexts/HideShowMenu";
+import Navbar from "../contexts/HideShowMenu";
 
 export default function Home() {
   return (
     <StickerProvider>
       <SearchBar />
       <IndexPage />
+      <Navbar></Navbar>
     </StickerProvider>
   );
 }
