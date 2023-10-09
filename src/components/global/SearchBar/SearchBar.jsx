23429a0b3c31eb7026f042ad87a6f605ff4390d8
Denis <prokhorend@gmail.com>
2023-10-09 13:55:53 +0300

    added mavigation menu

--- src/components/global/SearchBar/SearchBar.jsx
+++ src/components/global/SearchBar/SearchBar.jsx
@@ -1,29 +1,22 @@
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import styles from "./SearchBart.module.scss";
 
 export default function SearchBar() {
-    return (
-
+  return (
     <Container className={styles.wrapper}>
-        <div className={styles.searchBar}>
-            <div className={styles.searchImage}></div>
-            <input
-                className={styles.searchBox}
-                type="text"
-                placeholder="Search for stickers"
-            />
-            <div className={styles.infoBox}></div>
-        </div>
-        <div className={styles.menuBox}>
-            <div className={styles.menuItemBox}>
-                Новинки
-            </div>
-            <div className={styles.menuItemBox}>
-                Популярные
-            </div>
-        </div>
+      <div className={styles.searchBar}>
+        <div className={styles.searchImage}></div>
+        <input
+          className={styles.searchBox}
+          type="text"
+          placeholder="Search for stickers"
+        />
+        <div className={styles.infoBox}></div>
+      </div>
+      <div className={styles.menuBox}>
+        <div className={styles.menuItemBox}>Новинки</div>
+        <div className={styles.menuItemBox}>Популярные</div>
+      </div>
     </Container>
-
-
-);
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import IndexPage from "../components/page/Index/IndexPage";
 import SearchBar from "../components/global/SearchBar/SearchBar";
 import { StickerProvider } from "../contexts/StickerContext";
+import { ScrollTracker } from "../contexts/HideShowMenu";
 
 export default function Home() {
   return (
