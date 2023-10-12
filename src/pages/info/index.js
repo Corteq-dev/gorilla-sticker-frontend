7b70e79128564122cab94c9bd6903ab106c3eb0f
Denis <prokhorend@gmail.com>
2023-10-12 20:25:21 +0300

    changed cammalCase and refactored Page name

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -3,23 +3,23 @@ import { useEffect } from "react";
 import { Container } from "react-bootstrap";
 
 export default function Info() {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://telegram.org/js/telegram-web-app.js";
-        script.async = true;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-web-app.js";
+    script.async = true;
 
-        script.addEventListener("load", () => {
-            document
-                .querySelector("html")
-                .classList.add(window.Telegram.WebApp.colorScheme);
-        });
+    script.addEventListener("load", () => {
+      document
+        .querySelector("html")
+        .classList.add(window.Telegram.WebApp.colorScheme);
+    });
 
-        document.body.appendChild(script);
+    document.body.appendChild(script);
 
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <Container>
       <InfoPage></InfoPage>
