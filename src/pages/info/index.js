0afdb57a0b7ec9ca3d34e365c1b091f405d84f5b
Vlady <vlady@onlinedata.dk>
2023-10-24 14:09:29 +0200

    Deployment fix

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -1,6 +1,5 @@
 import InfoPage from "../../components/page/Info/InfoPage.jsx";
 import { Container } from "react-bootstrap";
-import SearchBar from "../../components/global/SearchBar/SearchBar";
 
 export default function Info() {
   return (
